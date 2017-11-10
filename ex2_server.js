a0c182b31354d148b0e4d8495931a56e0907b7b8
Karanpoj Varintaravet <31284177+zzomboy@users.noreply.github.com>
2017-11-10 11:08:07 +0700

    Add files via upload

--- ex2_server.js
+++ ex2_server.js
@@ -6,13 +6,16 @@ console.log(count);
 
 app.use(express.static(__dirname + "/client"));
 http.createServer(app).listen(3000);
+
 app.get("/count.json", function(req, res) {
     res.json(count);
 });
 
 app.post("/plus", function (req, res) {
-    console.log(req.body;
-    var newcount = req.body;
-    //console.log(newcount);
-    count=newcount;
+    count.qty++;
+    console.log(count);
+});
+app.post("/minus", function (req, res) {
+    count.qty--;
+    console.log(count);
 });
