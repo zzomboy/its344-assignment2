49b0d05fafcab6d83abff60af4d05ed115f23e65
Karanpoj Varintaravet <31284177+zzomboy@users.noreply.github.com>
2017-11-10 11:09:39 +0700

    Update app.js

--- client/app.js
+++ client/app.js
@@ -1,10 +1,10 @@
 var main = function (countObjects) {
     "use strict";
     var $qtytxt , $qtyshow , $minusbt , $addbt;
-
+    $(".content").empty();
     console.log(countObjects);
     var txt = countObjects['qty'];
-
+    
     $qtytxt = $("<p>").text("count").addClass('topic'),
     $qtyshow = $("<p>").text(txt).addClass('number'),
     $minusbt = $("<button>").text("-1").addClass('minusbt'),
@@ -12,10 +12,15 @@ var main = function (countObjects) {
     $(".content").append($qtytxt).append($qtyshow).append($minusbt).append($addbt);
     
     $addbt.on("click", function () {
-        countObjects['qty']++;
-        console.log(countObjects);
-        $.post('plus',function (request,response) {
-            console.log(request.body.number);
+        $.post('plus',function (response) {});
+        $.getJSON("count.json", function (countObjects) {
+            main(countObjects);
+        });
+    });
+    $minusbt.on("click", function () {
+        $.post('minus',function (response) {});
+        $.getJSON("count.json", function (countObjects) {
+            main(countObjects);
         });
     });
 };
